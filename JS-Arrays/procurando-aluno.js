19f8c125e871ec97db9935c98decd48c85b095f2
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2023-07-25 14:35:06 -0300

    Laços de repetição no JS

---
+++ JS-Arrays/procurando-aluno.js
@@ -0,0 +1,19 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias];
+
+function exibeNomeENota(aluno) {
+    if (listaDeAlunosEMedias[0].includes(aluno)) {
+        // const listaAlunos = listaDeAlunosEMedias[0];
+        // const listaMedias = listaDeAlunosEMedias[1];
+        const [listaAlunos, listaMedias] = listaDeAlunosEMedias;
+        const indice = listaAlunos.indexOf(aluno);
+        const mediaDoAluno = listaMedias[indice];
+        console.log(`${aluno} tem a nota média: ${mediaDoAluno}`);
+    } else {
+        console.log('Aluno não encontrado!');
+    }
+};
+
+exibeNomeENota('Caio');
