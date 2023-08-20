26ff267deab2daf8c8ebc67b1ed719937dabda84
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2023-08-20 14:18:10 -0300

    Objetos dentro de objetos em JS

---
+++ JS-Objetos/aninhados.js
@@ -0,0 +1,15 @@
+const cliente = {
+    nome: "Joao",
+    idade: 24,
+    emai: "joao@firma.com",
+    telefones: ["11955554444", "11944443000"]
+};
+
+cliente.endereco = {
+    rua: "Joseph Climber",
+    numero: 1337,
+    apartamento: true,
+    complemento: "Ap. 934"
+};
+
+console.log(cliente);
